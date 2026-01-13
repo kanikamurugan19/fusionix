35a39faecd311a0deb3d84a51c3db98fc7c34d90
kanikamurugan19 <kanikamurugan41@gmail.com>
2026-01-13 19:30:51 +0530

    Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -74,7 +74,7 @@ export function Footer() {
         {/* Copyright */}
         <div className="border-t border-gray-800 pt-8 text-center">
           <p className="text-gray-400">
-            © {new Date().getFullYear()} NEXO INFOTECH. All rights reserved.
+            © {new Date().getFullYear()} FUSIONIX TECHNOLOGIES. All rights reserved.
           </p>
         </div>
       </div>
